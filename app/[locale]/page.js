eccc5a5df6702b71b8dcf288af4c2b22af0acbb8
vetscan <vetscan.ct@gmail.com>
2026-07-13 16:28:42 +0300

    Update page.js

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -37,7 +37,7 @@ export default function Home() {
             {/* Розгорнутий SEO контент */}
             <div className={styles.seoContent}
     style={{
-    maxHeight: isExpanded ? 'none' : '180px',
+    maxHeight: isExpanded ? 'none' : '170px',
     overflow: 'hidden'
   }}
 >
